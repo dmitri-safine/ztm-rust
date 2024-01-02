36a4cb86b748820a80c2789c8eb16a6b7aacd372
Dmitri Safine <dsafine@tales.social>
2024-01-02 12:57:23 -0800

    a7 done

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -10,4 +10,28 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Colour {
+    Black,
+    Green,
+    Blue,
+    Red,
+    Purple,
+}
+
+fn print_colour(colour: Colour) {
+    match colour {
+        Colour::Black => println!("black"),
+        Colour::Green => println!("green"),
+        Colour::Blue => println!("blue"),
+        Colour::Red => println!("red"),
+        Colour::Purple => println!("purple"),
+    }
+}
+
+fn main() {
+    print_colour(Colour::Red);
+    print_colour(Colour::Black);
+    print_colour(Colour::Green);
+    print_colour(Colour::Blue);
+    print_colour(Colour::Purple);
+}
