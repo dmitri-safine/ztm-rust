688ad2c1d8743ac433eab27bb0e45f3a3d9ea921
Dmitri Safine <dsafine@tales.social>
2024-01-03 11:16:00 -0800

    a11 done

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -9,4 +9,22 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GroceryItem {
+    id: i32,
+    quantity: i32,
+}
+
+fn display_quantity(item: &GroceryItem) {
+    println!("quantity: {:?}", item.quantity);
+}
+
+fn display_id(item: &GroceryItem) {
+    println!("id: {:?}", item.id);
+}
+
+fn main() {
+    let item = GroceryItem { id: 1, quantity: 2 };
+
+    display_id(&item);
+    display_quantity(&item);
+}
