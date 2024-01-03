3fa78b8afc9a0f3f317d0ebb44b4174b70e2ded5
Dmitri Safine <dsafine@tales.social>
2024-01-03 12:09:53 -0800

    a12 done

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,4 +10,53 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Colour {
+    Green,
+    Blue,
+    Red,
+}
+
+impl Colour {
+    fn print(&self) {
+        match self {
+            Colour::Green => println!("colour: green"),
+            Colour::Blue => println!("colour: blue"),
+            Colour::Red => println!("colour: red"),
+        }
+    }
+}
+
+struct ShippingBox {
+    dimensions: (i32, i32, i32),
+    weight: f64,
+    colour: Colour,
+}
+
+impl ShippingBox {
+    fn new(dimensions: (i32, i32, i32), weight: f64, colour: Colour) -> Self {
+        ShippingBox {
+            dimensions,
+            weight,
+            colour,
+        }
+    }
+
+    fn print_characteristics(&self) {
+        println!(
+            "dimensions: {:?}x{:?}x{:?}",
+            self.dimensions.0, self.dimensions.1, self.dimensions.2
+        );
+        println!("weight: {:?}", self.weight);
+        self.colour.print();
+    }
+}
+fn main() {
+    let blue_box = ShippingBox::new((1, 2, 3), 45.4, Colour::Blue);
+    blue_box.print_characteristics();
+
+    let red_box = ShippingBox::new((4, 5, 6), 12.1, Colour::Red);
+    red_box.print_characteristics();
+
+    let green_box = ShippingBox::new((7, 8, 9), 65.3, Colour::Green);
+    green_box.print_characteristics();
+}
