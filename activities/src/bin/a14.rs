2818f3373dfdd043019001d8935d460ffb900a8f
Dmitri Safine <dsafine@tales.social>
2024-01-05 14:20:20 -0800

    a14 done

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,38 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: u8,
+    name: String,
+    fav_colour: String,
+}
+
+fn print(data: &str) {
+    println!("{:?}", data);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            age: 25,
+            name: "John".to_owned(),
+            fav_colour: "red".to_owned(),
+        },
+        Person {
+            age: 10,
+            name: "Jimmy".to_owned(),
+            fav_colour: "blue".to_owned(),
+        },
+        Person {
+            age: 7,
+            name: "Johnny".to_owned(),
+            fav_colour: "green".to_owned(),
+        },
+    ];
+    for person in people {
+        if person.age <= 10 {
+            print(&person.name);
+            print(&person.fav_colour);
+        }
+    }
+}
